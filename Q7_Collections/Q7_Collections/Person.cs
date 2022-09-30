8652be153c02c131ebef0f7d8e23e747c7eade57
Ron7978 <72132759+Ron7978@users.noreply.github.com>
2022-09-30 16:11:52 +0530

    Add files via upload

---
+++ Q7_Collections/Q7_Collections/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Q7_Collections
+{
+    class members
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+    internal class Person
+    {
+        Queue<members> queue;
+        public Person()
+        {
+            queue = new Queue<members>();
+
+        }
+
+        public void ToString(members members)
+        {
+            queue.Enqueue(members);
+            Thread.Sleep(1000);
+            Console.WriteLine($"Name: {members.Name} Age: {members.Age}\n");
+        }
+
+        public void ToProcess()
+        {
+            if (queue.Any())
+            {
+                var members = queue.Dequeue();
+                Thread.Sleep(1000);
+                Console.WriteLine($"Processed\nName: {members.Name} Age: {members.Age}");
+            }
+        }
+
+        public void ShowQueue()
+        {
+            Console.WriteLine("\nCurrent queue\n");
+            foreach (var person in queue)
+            {
+                Thread.Sleep(500);
+                members m = new members();
+                Console.WriteLine(m.Name);
+                Console.WriteLine(m.Age);
+            }
+        }
+    }
+}
---
+++ Q7_Collections/Q7_Collections/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q7_Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.ToString(new members { Name = "Rounak", Age = 22 });
+            p.ToString(new members { Name = "Sonali", Age = 21 });
+            p.ToString(new members { Name = "Akshat", Age = 23 });
+            p.ToString(new members { Name = "sameer", Age = 30 });
+            p.ToString(new members { Name = "Rony", Age = 20 });
+
+            p.ToProcess();
+            p.ShowQueue(); 
+            Console.ReadKey();
+        }
+    }
+}
