a7231ad1618c5c6347610084d5352f238b233b22
Ron7978 <72132759+Ron7978@users.noreply.github.com>
2022-09-30 15:56:18 +0530

    Add files via upload
    Question 1

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1_min_max_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Enter the elements of the array : ");
+            for (int i = 0; i < arr.Length; i++)
+            { 
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Minimum number is " + arr.Min());
+            Console.WriteLine("Maximum number is " + arr.Max());
+            Console.ReadLine();
+            
+        }
+    }
+}
