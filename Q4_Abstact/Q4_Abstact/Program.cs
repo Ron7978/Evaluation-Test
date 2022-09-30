79fa3f699a8f99d0a22a90273d8188b890ac6f07
Ron7978 <72132759+Ron7978@users.noreply.github.com>
2022-09-30 16:08:36 +0530

    Add files via upload

---
+++ Q4_Abstact/Q4_Abstact/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q4_Abstact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the name of bird : ");
+            parrot dog = new parrot();
+            //Console.WriteLine("Name : ");
+            dog.SetName(Console.ReadLine());
+            Console.WriteLine("Status : ");
+            Console.WriteLine(dog.GetName());
+            dog.Fly();
+
+            Console.ReadLine();
+        }
+
+        public class parrot : Bird
+        {
+            public override void Fly()
+            {
+                Console.WriteLine("Flying");
+            }
+        }
+
+        public abstract class Bird
+        {
+            private string Name;
+
+            public void SetName(string name)
+            {
+                Name = name;
+            }
+
+            public string GetName()
+            {
+                return Name;
+            }
+
+            public abstract void Fly();
+        }
+    }
+}
